8f325e9a413c73bc36f5714782c11ac47b71061b
Herbert W. Xin <fihnex@proton.me>
2024-07-03 17:49:52 +1000

    Delete Simple Model directory

--- Simple Model/s_shape.m
+++
@@ -1,8 +0,0 @@
-suptitle('Dyanmics of v, vstar and s');
-set(gcf, 'Position', [100, 100, 1000, 600]);
-fig=axes(gcf,'visible','off'); 
-fig.Title.Visible='on';
-fig.XLabel.Visible='on';
-fig.YLabel.Visible='on';
-ylabel(fig,'standard deviation');
-xlabel(fig,'period')
